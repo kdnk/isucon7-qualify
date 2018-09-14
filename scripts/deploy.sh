77329cf4f1aab06df3c5f18a9033ab01e45409e9
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2018-09-14 21:02:22 +0900

    Fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ set -o pipefail
 CMD="
 cd ~/isubata && \
 git pull origin master && \
-cp ~/isubata/nginx.conf /etc/nginx/sites-enabled/nginx.conf && \
+sudo cp ~/isubata/nginx.conf /etc/nginx/sites-enabled/nginx.conf && \
 cd ~/isubata/webapp/nodejs && \
 sudo systemctl restart nginx && \
 sudo systemctl restart isubata.nodejs.service
